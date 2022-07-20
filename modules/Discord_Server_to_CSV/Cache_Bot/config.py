1e100167d9181789f98c127740d49b541c6387dc
kjwallace <72354709+kjwallace@users.noreply.github.com>
2022-07-20 15:13:54 -0400

    Update config.py

--- modules/Discord_Server_to_CSV/Cache_Bot/config.py
+++ modules/Discord_Server_to_CSV/Cache_Bot/config.py
@@ -2,7 +2,7 @@ import os
 
 def Config():
     config={
-            "token":'OTY3Mjc0MjAzNjUwODYzMTA0.YmN6TQ.XKsFaC4PtW7GkBFXnTtYJC0OKoE',
+            "token":'token',
             "command_pref":"!!",
             "permissions":8,
             "output_url" : f'{os.path.dirname(os.path.realpath(__name__))}/Output_CSVs'
